9531b719fb5f5c184a99eac69c09bb9b9883c35f
dilshodbek0505 <ddilshod0415@fmail.com>
2024-05-06 15:56:15 +0500

    settings filedagi xatolik to'g'rilandi

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0rwuxa3a1d93z%6q!*hk6)tfl=*f&q@opph7e3i3&3%v+h9tr=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
